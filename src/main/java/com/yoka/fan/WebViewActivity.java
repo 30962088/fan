a53c43d935ea7c0602d6280da9d646f8d0fd7aed
poolk001@qq.com <william>
2014-03-14 23:25:49 +0800

    update

--- src/main/java/com/yoka/fan/WebViewActivity.java
+++ src/main/java/com/yoka/fan/WebViewActivity.java
@@ -2,9 +2,10 @@ package com.yoka.fan;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.KeyEvent;
-import android.webkit.WebChromeClient;
+import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -14,22 +15,29 @@ public class WebViewActivity extends BaseActivity{
 	
 	private WebView webView;
 	
+	
 	@Override
 	protected void onCreate(Bundle bundle) {
 		// TODO Auto-generated method stub
 		super.onCreate(bundle);
-		webView = new WebView(this);
-
+		setContentView(R.layout.webview_layout);
+		webView = (WebView) findViewById(R.id.webview);
+		final View loading = findViewById(R.id.loading);
 		webView.getSettings().setJavaScriptEnabled(true);
 		webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
 		webView.loadUrl(getIntent().getStringExtra(PARAM_URL));
 		webView.setWebViewClient(new WebViewClient() {
 	        @Override
 	        public void onPageFinished(WebView view, String url) {
+	        	loading.setVisibility(View.GONE);
 	            setWebTitle(view.getTitle());
 	        }
+	        @Override
+	        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+	        	loading.setVisibility(View.VISIBLE);
+	        	super.onPageStarted(view, url, favicon);
+	        }
 	    });
-		setContentView(webView);
 	}
 	
 	@Override
@@ -47,7 +55,11 @@ public class WebViewActivity extends BaseActivity{
 	@Override
 	public boolean onKeyDown(int keyCode, KeyEvent event) {
 		if (keyCode == KeyEvent.KEYCODE_BACK) {
-			
+			if(webView.canGoBack()){
+				webView.goBack();
+			}else{
+				finish();
+			}
 			return true;
 		}
 		return super.onKeyDown(keyCode, event);
