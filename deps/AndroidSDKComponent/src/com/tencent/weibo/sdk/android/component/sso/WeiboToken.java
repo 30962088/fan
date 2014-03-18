186525a3dfde9a037f318de5ddf6c0b95527039f
poolk001@qq.com <william>
2014-03-18 23:40:10 +0800

    update

---
+++ deps/AndroidSDKComponent/src/com/tencent/weibo/sdk/android/component/sso/WeiboToken.java
@@ -0,0 +1,31 @@
+package com.tencent.weibo.sdk.android.component.sso;
+
+import java.io.Serializable;
+
+/**
+ * 令牌
+ * 
+ * @author isaacxie
+ * 
+ */
+public class WeiboToken implements Serializable{
+
+	/** AccessToken */
+	public String accessToken;
+
+	/** AccessToken过期时间 */
+	public long expiresIn;
+
+	/** 用来换新的AccessToken */
+	public String refreshToken;
+
+	/** 兼容OpenID协议 */
+	public String openID;
+
+	/** OmasToken */
+	public String omasToken;
+
+	/** omasKey */
+	public String omasKey;
+
+}
