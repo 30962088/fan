70abbf1dae2226d4375683dd80c46f4d12eb522c
poolk001@qq.com <william>
2014-03-23 15:14:58 +0800

    update

--- src/main/java/com/yoka/fan/utils/CacheManager.java
+++ src/main/java/com/yoka/fan/utils/CacheManager.java
@@ -55,12 +55,15 @@ public class CacheManager {
 
 	public static long folderSize(File directory) {
 		long length = 0;
-		for (File file : directory.listFiles()) {
-			if (file.isFile())
-				length += file.length();
-			else
-				length += folderSize(file);
+		if(directory != null){
+			for (File file : directory.listFiles()) {
+				if (file.isFile())
+					length += file.length();
+				else
+					length += folderSize(file);
+			}
 		}
+		
 		return length;
 	}
 }
